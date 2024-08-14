3e8d89fb3590c9eb38d6f6e4fa99dcb6269da580
Nguyễn Dự Hiếu <duhieu2806@gmail.com>
2024-08-14 23:57:57 +0700

    san pham

--- back-end/src/main/java/com/example/backend/service/SanPhamService.java
+++ back-end/src/main/java/com/example/backend/service/SanPhamService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 public class SanPhamService {
-    //ok
+    //ok123
     @Autowired
     SanPhamRepository sanPhamRepository;
     public List<SanPham> getALL(){
